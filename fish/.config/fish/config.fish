799411b6a7daa07c34cd69a12653b524f408542f
Luis Ramos <luismeramosr@gmail.com>
2022-08-19 13:51:21 -0500

    Repo from scratch

---
+++ fish/.config/fish/config.fish
@@ -0,0 +1,18 @@
+if status is-interactive
+  # Commands to run in interactive sessions can go here
+  set --export JAVA_HOME /lib/jvm/java-11-openjdk;
+  set --export ANDROID $HOME/Android;
+  set --export ANDROID_HOME $ANDROID/Sdk;
+  set --export CHROME_EXECUTABLE /usr/bin/brave;
+  set --export GOPATH ~/go;
+  set --export jupyter /opt/anaconda/bin/jupyter;
+  set -gx PATH $ANDROID_HOME/tools $PATH;
+  set -gx PATH $ANDROID_HOME/tools/bin $PATH;
+  set -gx PATH $ANDROID_HOME/platform-tools $PATH;
+  set -gx PATH $ANDROID_HOME/emulator $PATH;
+  set -gx PATH /opt/flutter/bin/ $PATH;
+  set -gx PATH $GOPATH/bin $PATH;
+  set -gx PATH ~/.cargo/bin $PATH;
+  set -gx PATH ~/.yarn/bin $PATH;
+  set fish_greeting
+end
