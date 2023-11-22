d2d049782b340a0e05d91406005bf6a17e6471e4
Luis Ramos <luismeramosr@gmail.com>
2023-11-22 09:28:47 -0500

    Changed Fish config to see use Windows Android Sdk

--- fish/.config/fish/config.fish
+++ fish/.config/fish/config.fish
@@ -1,14 +1,14 @@
 if status is-interactive
   # Commands to run in interactive sessions can go here
   set --export JAVA_HOME /lib/jvm/java-17-openjdk;
-  set --export ANDROID /mnt/c/Users/luisr/AppData/Local/Android;
-  set --export ANDROID_HOME $ANDROID/Sdk;
+  # set --export ANDROID /mnt/c/Users/luisr/AppData/Local/Android;
+  # set --export ANDROID_HOME $ANDROID/Sdk;
   set --export CHROME_EXECUTABLE /usr/bin/brave;
   set --export jupyter /opt/anaconda/bin/jupyter;
-  set -gx PATH $ANDROID_HOME/tools $PATH;
-  set -gx PATH $ANDROID_HOME/tools/bin $PATH;
-  set -gx PATH $ANDROID_HOME/platform-tools $PATH;
-  set -gx PATH $ANDROID_HOME/emulator $PATH;
+  # set -gx PATH $ANDROID_HOME/tools $PATH;
+  # set -gx PATH $ANDROID_HOME/tools/bin $PATH;
+  # set -gx PATH $ANDROID_HOME/platform-tools $PATH;
+  # set -gx PATH $ANDROID_HOME/emulator $PATH;
   set -gx PATH /opt/flutter/bin/ $PATH;
   set -gx PATH ~/.cargo/bin $PATH;
   set -gx PATH ~/.yarn/bin $PATH;
