e453e191a7c0c594488c7a8b8033ab0d14ba9afe
Luis Ramos <luismeramosr@gmail.com>
2024-01-10 13:30:39 -0500

    Changing alacritty font, fish android_home path

--- fish/.config/fish/config.fish
+++ fish/.config/fish/config.fish
@@ -1,6 +1,6 @@
 # Commands to run in interactive sessions can go here
 set --export JAVA_HOME /lib/jvm/java-21-openjdk;
-set --export ANDROID /mnt/c/Users/luisr/AppData/Local/Android;
+set --export ANDROID ~/Android;
 set --export ANDROID_HOME $ANDROID/Sdk;
 set --export CHROME_EXECUTABLE /usr/bin/brave;
 set --export GOPATH ~/go;
